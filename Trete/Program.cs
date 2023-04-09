2ccbb73acb9171e82c3640f9c22ca015bddecf59
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-09 09:53:43 +0300

    dz3 osn

--- Trete/Program.cs
+++ Trete/Program.cs
@@ -3,3 +3,10 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+int Bass(string sms)
+{
+    System.Console.WriteLine(sms);
+    string readImput = System.Console.ReadLine();
+    int result = int.Parse(readImput);
+    return result; 
+}
