c5681040e6383e862ee907139fa96c4de4108b82
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-09 08:28:01 +0300

    dz1 2

--- Pervoe/Program.cs
+++ Pervoe/Program.cs
@@ -12,3 +12,14 @@ int Bass(string sms)
     return result; 
 }
 
+int Vozvedenie(int znachA, int exponentB)
+{
+    int znach = 1;
+    for (int i = 0; i < exponentB; i++)
+    {
+        znach *= znachA;
+    }
+    return znach;
+
+}
+
