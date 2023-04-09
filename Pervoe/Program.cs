880b3b03d372f3bd18a5bd88cfe75533d4855340
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-09 08:12:38 +0300

    dz1 vvod

--- Pervoe/Program.cs
+++ Pervoe/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int Bass(string sms)
+{
+    System.Console.WriteLine(sms);
+    string readImput = System.Console.ReadLine();
+    int result = int.Parse(readImput);
+    return result; 
+}
+
