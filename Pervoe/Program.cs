79d07d0c1aee75db37a74b4a074699543d315f43
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-09 08:42:32 +0300

    dz1 2

--- Pervoe/Program.cs
+++ Pervoe/Program.cs
@@ -23,3 +23,6 @@ int Vozvedenie(int znachA, int exponentB)
 
 }
 
+int znachA = Bass("vvedite osnovanie: ");
+int exponentB = Bass("Vvedite Pokazatel: ");
+System.Console.WriteLine($"Cheslo {znachA} v stepeni {exponentB} ravno {Bass(znachA, exponentB)}");
